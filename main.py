680e1c191ef7ad455f4c271534fb43b736c524e3
anitejb <41495799+anitejb@users.noreply.github.com>
2020-09-04 00:31:54 -0400

    created groups with leaders and students
    Co-authored-by: mmatlin <33261504+mmatlin@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
-import itertools
-import time
 import random
+import time
 
 # TODO: Remove column names and value constants from functions (e.g. "RUID", "Available", "Yes")
 
@@ -10,7 +9,9 @@ INTERVAL_BOUNDS = ("8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4
 TIME_SLOTS = tuple(
     f"{day} {times}"
     for day in DAY_NAMES
-    for times in (f"{start} - {end}" for start, end in zip(INTERVAL_BOUNDS, INTERVAL_BOUNDS[1:]))
+    for times in (
+        f"{start} - {end}" for start, end in zip(INTERVAL_BOUNDS, INTERVAL_BOUNDS[1:])
+    )
 )
 
 
@@ -22,7 +23,9 @@ def create_roster(csv_filename):
         for _ in range(lines_to_skip):
             next(csvfile)
 
-        group_leader_column_name = "Were you confirmed to be a Discussion Group Facilitator?"
+        group_leader_column_name = (
+            "Were you confirmed to be a Discussion Group Facilitator?"
+        )
         roster = dict()
         leader_ids = set()
         student_ids = set()
@@ -36,6 +39,7 @@ def create_roster(csv_filename):
                 for column_name, value in row.items()
                 if column_name in TIME_SLOTS
             }
+
             if row[group_leader_column_name] == "Yes":
                 leader_ids.add(row["RUID"])
             else:
@@ -51,10 +55,24 @@ def create_roster(csv_filename):
                 "leader": row[group_leader_column_name] == "Yes",
                 "availability": availability,
             }
+
     return roster, leader_ids, student_ids
 
 
 def create_time_slot_buckets(roster, leader_ids, student_ids):
+    """Example time_slot_buckets dict:
+
+    time_slot_buckets = {
+        "time_name_1": {
+            "leader_ids": {leader_id_1, leader_id_2, ...},
+            "student_ids": {student_id_1, student_id_2, ...},
+        },
+        "time_name_2": {
+            "leader_ids": {leader_id_1, leader_id_3, ...},
+            "student_ids": {student_id_6, student_id_8, ...},
+        },
+    }
+    """
     # Initialize time slots
     time_slot_buckets = dict()
     for slot in TIME_SLOTS:
@@ -81,7 +99,10 @@ def remove_unavailable_time_slots(time_slot_buckets, leader_ids, student_ids):
     available_student_ids = set()
     all_time_slots = [key for key in time_slot_buckets.keys()]
     for slot in all_time_slots:
-        if not time_slot_buckets[slot]["leader_ids"] or not time_slot_buckets[slot]["student_ids"]:
+        if (
+            not time_slot_buckets[slot]["leader_ids"]
+            or not time_slot_buckets[slot]["student_ids"]
+        ):
             del time_slot_buckets[slot]
         else:
             available_leader_ids.update(time_slot_buckets[slot]["leader_ids"])
@@ -99,10 +120,98 @@ def remove_unavailable_time_slots(time_slot_buckets, leader_ids, student_ids):
         # At least one student exists who does not fit into any time slot
         student_ids = available_student_ids
 
-    return time_slot_buckets, leader_ids, student_ids, unavailable_leader_ids, unavailable_student_ids
+    return (
+        time_slot_buckets,
+        leader_ids,
+        student_ids,
+        unavailable_leader_ids,
+        unavailable_student_ids,
+    )
 
 
-def create_groups(time_slot_buckets, roster, leader_ids, student_ids):
+def create_leader_schedule(time_slot_buckets, leader_ids, student_ids):
+    timeout = 5
+    min_missing_students = student_ids
+    schedules = []
+
+    start = time.time()
+    while time.time() - start < timeout:
+        possible_slots = dict()
+        filled_students = set()
+
+        shuffled_leader_ids = list(leader_ids)
+        random.shuffle(shuffled_leader_ids)
+
+        shuffled_time_slots = list(time_slot_buckets.keys())
+        random.shuffle(shuffled_time_slots)
+
+        for slot in shuffled_time_slots:
+            for leader_id in shuffled_leader_ids:
+                if leader_id in time_slot_buckets[slot]["leader_ids"]:
+                    possible_slots[slot] = leader_id
+
+                    for student_id in time_slot_buckets[slot]["student_ids"]:
+                        filled_students.add(student_id)
+
+                    shuffled_leader_ids.remove(leader_id)
+                    break
+
+        missing_students = student_ids - filled_students
+
+        if len(missing_students) < len(min_missing_students):
+            min_missing_students = missing_students
+            schedules = [possible_slots]
+        elif len(missing_students) == len(min_missing_students):
+            schedules.append(possible_slots)
+
+        if len(missing_students) == 0:
+            break
+
+    return schedules, min_missing_students
+
+
+def fill_students(time_slot_buckets, student_ids, schedules):
+    best_final_schedule = {}
+    best_unavailable_student_ids = student_ids
+
+    for schedule in schedules:
+        final_schedule = {}
+        unavailable_student_ids = set()
+
+        for slot in schedule:
+            final_schedule[slot] = {
+                "leader_ids": set([schedule[slot]]),
+                "student_ids": set(),
+            }
+
+        student_ids_with_slots = []
+        for student_id in student_ids:
+            slots = []
+            for slot in schedule:
+                if student_id in time_slot_buckets[slot]["student_ids"]:
+                    slots.append(slot)
+            student_ids_with_slots.append((student_id, slots))
+        student_ids_with_slots.sort(key=lambda x: len(x[1]))
+
+        for student_id, slots in student_ids_with_slots:
+            if len(slots) == 0:
+                unavailable_student_ids.add(student_id)
+            elif len(slots) == 1:
+                final_schedule[slots[0]]["student_ids"].add(student_id)
+            else:
+                lowest_slot = min(
+                    slots, key=lambda slot: len(final_schedule[slot]["student_ids"])
+                )
+                final_schedule[lowest_slot]["student_ids"].add(student_id)
+
+        if len(unavailable_student_ids) < len(best_unavailable_student_ids):
+            best_final_schedule = final_schedule
+            best_unavailable_student_ids = unavailable_student_ids
+
+    return best_final_schedule, best_unavailable_student_ids
+
+
+def create_groups(time_slot_buckets, leader_ids, student_ids):
     """
     —All leaders and students must be scheduled for one (1) time slot which they are available for
     —No two leaders can be scheduled for the same time slot
@@ -113,54 +222,98 @@ def create_groups(time_slot_buckets, roster, leader_ids, student_ids):
     while True:
         starting_time = time.time()
         while time.time() - starting_time < attempt_interval:
-            schedule = {slot: {"leader_ids": set(), "student_ids": set()} for slot in time_slot_buckets.keys()}
-            
+            break_out = False
+            schedule = {
+                slot: {"leader_ids": set(), "student_ids": set()}
+                for slot in time_slot_buckets.keys()
+            }
+            for leader_id in leader_ids:
+                # destination_slot = random.choice(filter(lambda slot: len(schedule[slot]["leader_ids"]) == 0, schedule.keys()))
+
+                possible_schedule = []
+                for slot in schedule.keys():
+                    if (
+                        leader_id in time_slot_buckets[slot]["leader_ids"]
+                        and len(schedule[slot]["leader_ids"]) == 0
+                    ):
+                        possible_schedule.append(slot)
+
+                destination_slot = random.choice(possible_schedule)
+                schedule[destination_slot]["leader_ids"].add(leader_id)
+
+            for student_id in student_ids:
+                # destination_slot = random.choice(schedule.keys())
+                # destination_slot = random.choice([slot for slot in schedule.keys() if len(schedule[slot]["leader_ids"]) == 1])
+
+                possible_schedule = []
+                for slot in schedule.keys():
+                    if (
+                        student_id in time_slot_buckets[slot]["student_ids"]
+                        and len(schedule[slot]["leader_ids"]) == 1
+                    ):
+                        possible_schedule.append(slot)
+
+                if possible_schedule:
+                    destination_slot = random.choice(possible_schedule)
+                    schedule[destination_slot]["student_ids"].add(student_id)
+                else:
+                    break_out = True
+                    break
+
+            if break_out:
+                break
+
+            print("new schedule")
+            if is_valid_schedule(schedule, time_slot_buckets):
+                return schedule
         max_members_diff += 1
+        print(max_members_diff)
 
-
-def find_group_leader_options(time_slot_buckets, leader_ids):
-    for slot in time_slot_buckets:
-        time_slot_buckets[slot]["curr_index"] = 0
-
-    schedule = dict()
-    filling_leader_ids = set()
-    slots = itertools.cycle(time_slot_buckets.keys())
-
-    while True:
-        slot = next(slots)
-        curr_index = time_slot_buckets[slot]["curr_index"]
-        curr_leader_id = time_slot_buckets[slot][leader_ids][curr_index]
-        while curr_index < len(time_slot_buckets[slot][leader_ids]):
-            if curr_leader_id not in filling_leader_ids:
-                filling_leader_ids.append(time_slot_buckets[slot][leader_ids][curr_index])
-                schedule[slot] = curr_leader_id
-                time_slot_buckets[slot]["curr_index"] += 1
-            else:
-                time_slot_buckets[slot]["curr_index"] += 1
-            if filling_leader_ids == leader_ids:
-                yield schedule
-                schedule = dict()
-                filling_leader_ids = set()
 
 def is_valid_schedule(schedule, availability_buckets):
     people_already_encountered = set()
-    for slot, data in schedule.items():
+    for schedule_slot, schedule_slot_data in schedule.items():
         # Check that there are no instances of students being scheduled with a non-1 amount of leaders
-        if len(data["leader_ids"]) != 1 and len(data["student_ids"]):
+        if len(schedule_slot_data["leader_ids"]) != 1 and len(
+            schedule_slot_data["student_ids"]
+        ):
             return False
         # Check that there are no instances of a leader being scheduled with no students
-        elif len(data["leader_ids"]) == 1 and not len(data["student_ids"]):
+        elif len(schedule_slot_data["leader_ids"]) == 1 and not len(
+            schedule_slot_data["student_ids"]
+        ):
             return False
         # Check that no person is scheduled twice
-        if any([person in people_already_encountered for person in (data["leader_ids"] + data["student_ids"])]):
+        if any(
+            [
+                person in people_already_encountered
+                for person in (
+                    schedule_slot_data["leader_ids"] | schedule_slot_data["student_ids"]
+                )
+            ]
+        ):
             return False
         # Check that no student is scheduled for when they are unavailable
-        if any([student not in availability_buckets[slot]["student_ids"]]):
+        if any(
+            [
+                student not in availability_buckets[schedule_slot]["student_ids"]
+                for student in schedule_slot_data["student_ids"]
+            ]
+        ):
             return False
         # Check that no leader is scheduled for when they are unavailable
-        if any([leader not in availability_buckets[slot]["leader_ids"]]):
+        if any(
+            [
+                leader not in availability_buckets[schedule_slot]["leader_ids"]
+                for leader in schedule_slot_data["leader_ids"]
+            ]
+        ):
             return False
-        
+        people_already_encountered.update(
+            schedule_slot_data["leader_ids"] | schedule_slot_data["student_ids"]
+        )
+    return True
+
 
 def main():
     csv_filename = "real.csv"
@@ -172,20 +325,56 @@ def main():
     #   - Keys are Time Slot names,
     #   - Values are dicts containing available leaders and students for the given time slot
     time_slot_buckets = create_time_slot_buckets(roster, leader_ids, student_ids)
-    time_slot_buckets, leader_ids, student_ids, unavailable_leader_ids, unavailable_student_ids = remove_unavailable_time_slots(time_slot_buckets, leader_ids, student_ids)
+    (
+        time_slot_buckets,
+        leader_ids,
+        student_ids,
+        unavailable_leader_ids,
+        unavailable_student_ids,
+    ) = remove_unavailable_time_slots(time_slot_buckets, leader_ids, student_ids)
     if unavailable_leader_ids:
         print("Leaders who are not available at any time that students are available:")
         for unavailable_leader_id in unavailable_leader_ids:
-            print(f"{roster[unavailable_leader_id]['first']} {roster[unavailable_leader_id]['last']} (RUID: {unavailable_leader_id})")
+            print(
+                f"{roster[unavailable_leader_id]['first']} {roster[unavailable_leader_id]['last']} (RUID: {unavailable_leader_id})"
+            )
     if unavailable_student_ids:
-        print("Students who are not available at any time that students are available:")
+        print("Students who are not available at any time that leaders are available:")
         for unavailable_student_id in unavailable_student_ids:
-            print(f"{roster[unavailable_student_id]['first']} {roster[unavailable_student_id]['last']} (RUID: {unavailable_student_id})")
-    pass
-    # print(f"Leaders who are not available at any time that students are available:\n{"\n".join([
-    #     f'{roster[unavailable_leader_id]["""first"""]} {roster[unavailable_leader_id]["""last"""]} (RUID: {unavailable_leader_id})'
-    #     for unavailable_leader_id in unavailable_leader_ids
-    #     ])}")
+            print(
+                f"{roster[unavailable_student_id]['first']} {roster[unavailable_student_id]['last']} (RUID: {unavailable_student_id})"
+            )
+    # schedule = create_groups(time_slot_buckets, leader_ids, student_ids)
+    # print(schedule)
+    schedules, min_missing_students = create_leader_schedule(
+        time_slot_buckets, leader_ids, student_ids
+    )
+    final_schedule, unavailable_student_ids = fill_students(
+        time_slot_buckets, student_ids, schedules
+    )
+
+    print("Is valid:", is_valid_schedule(final_schedule, time_slot_buckets))
+
+    for slot in final_schedule:
+        (leader_id,) = final_schedule[slot]["leader_ids"]
+        print("Time:", slot)
+        print(
+            "Leader:",
+            f"{roster[leader_id]['first']} {roster[leader_id]['last']} (RUID: {leader_id})",
+        )
+        print("Students:", len(final_schedule[slot]["student_ids"]))
+        for student_id in final_schedule[slot]["student_ids"]:
+            print(
+                f"{roster[student_id]['first']} {roster[student_id]['last']} (RUID: {student_id})"
+            )
+        print()
+
+    if unavailable_student_ids:
+        print("Students who have conflicts:")
+        for unavailable_student_id in unavailable_student_ids:
+            print(
+                f"{roster[unavailable_student_id]['first']} {roster[unavailable_student_id]['last']} (RUID: {unavailable_student_id})"
+            )
 
 
 if __name__ == "__main__":
